5cbecaccae88d3f5cd0ffbf06d295aa6d8ed88a6
navigaid <navigaid@gmail.com>
2025-01-27 23:50:31 +0000

    web: add vscode-web-only target
    gulp.task

--- build/buildfile.ts
+++ build/buildfile.ts
@@ -51,6 +51,7 @@ export const code = [
 ];
 
 export const codeWeb = createModuleDescription('vs/code/browser/workbench/workbench');
+export const codeWebOnly = createModuleDescription('vs/code/browser/workbench/web-only');
 
 export const codeServer = [
 	// 'vs/server/node/server.main' is not included here because it gets inlined via ./src/server-main.js
@@ -79,6 +80,7 @@ const buildfile = {
 	keyboardMaps,
 	code,
 	codeWeb,
+	codeWebOnly,
 	codeServer,
 	entrypoint: createModuleDescription
 };
