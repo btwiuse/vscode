b0cce73068dbbba6a93a3ab82c1462968f8c6dd4
navigaid <navigaid@gmail.com>
2025-01-27 22:18:46 +0000

    web: add web target to gulpfile.reh.js
    fix build/gulpfile.reh.js

--- build/gulpfile.reh.ts
+++ build/gulpfile.reh.ts
@@ -25,7 +25,7 @@ import { untar } from './lib/util.ts';
 import File from 'vinyl';
 import * as fs from 'fs';
 import glob from 'glob';
-import { compileBuildWithManglingTask } from './gulpfile.compile.ts';
+import { compileBuildWithManglingTask, compileBuildWithoutManglingTask } from './gulpfile.compile.ts';
 import { cleanExtensionsBuildTask, compileNonNativeExtensionsBuildTask, compileNativeExtensionsBuildTask, compileExtensionMediaBuildTask } from './gulpfile.extensions.ts';
 import { vscodeWebResourceIncludes, createVSCodeWebFileContentMapper } from './gulpfile.vscode.web.ts';
 import * as cp from 'child_process';
@@ -486,3 +486,31 @@ function tweakProductForServerWeb(product: typeof import('../product.json')) {
 		});
 	});
 });
+
+// Web specific build task
+const bundleWebTask = task.define('bundle-web', task.series(
+    util.rimraf('out-web'),
+    optimize.bundleTask(
+        {
+            out: 'out-web',
+            esm: {
+                src: 'out-build',
+                entryPoints: [
+                    buildfile.codeWeb
+                ].flat(),
+                resources: [
+                    'out-build/vs/code/browser/workbench/*.html',
+                    ...vscodeWebResourceIncludes,
+                    '!out-build/vs/code/**/*-dev.html'
+                ],
+                fileContentMapper: createVSCodeWebFileContentMapper('.build/extensions', product)
+            }
+        }
+    )
+));
+
+// Main web build tasks
+gulp.task('web', task.series(
+    compileBuildWithoutManglingTask,
+    bundleWebTask
+));
