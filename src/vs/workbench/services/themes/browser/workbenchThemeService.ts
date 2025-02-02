f2813fb5f8307ebc2fd312ec36b756b65278c806
navigaid <navigaid@gmail.com>
2025-02-02 09:30:00 +0000

    revert color scheme hack

--- src/vs/workbench/services/themes/browser/workbenchThemeService.ts
+++ src/vs/workbench/services/themes/browser/workbenchThemeService.ts
@@ -153,7 +153,7 @@ export class WorkbenchThemeService extends Disposable implements IWorkbenchTheme
 			}
 		}
 		if (!themeData) {
-			const colorScheme = this.settings.getPreferredColorScheme() ?? (isWeb ? ColorScheme.DARK : ColorScheme.LIGHT);
+			const colorScheme = this.settings.getPreferredColorScheme() ?? (isWeb ? ColorScheme.LIGHT : ColorScheme.DARK);
 			themeData = ColorThemeData.createUnloadedThemeForThemeType(colorScheme, defaultColorMap);
 		}
 		themeData.setCustomizations(this.settings);
