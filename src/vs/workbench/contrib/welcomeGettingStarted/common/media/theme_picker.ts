11dbf5a8b8acd801145e86d9ce16b843903430bc
navigaid <navigaid@gmail.com>
2025-04-20 08:50:01 +0000

    add color blind themes

--- src/vs/workbench/contrib/welcomeGettingStarted/common/media/theme_picker.ts
+++ src/vs/workbench/contrib/welcomeGettingStarted/common/media/theme_picker.ts
@@ -10,6 +10,16 @@ import { ThemeSettingDefaults } from '../../../../services/themes/common/workben
 export default () => `
 <checklist>
 	<div class="theme-picker-row">
+		<checkbox when-checked="setTheme:${ThemeSettingDefaults.COLOR_THEME_DARK_COLORBLIND}" checked-on="config.workbench.colorTheme == '${ThemeSettingDefaults.COLOR_THEME_DARK_COLORBLIND}'">
+			<img width="200" src="./dark-colorblind.png"/>
+			${escape(localize('dark-colorblind', "Dark Colorblind"))}
+		</checkbox>
+		<checkbox when-checked="setTheme:${ThemeSettingDefaults.COLOR_THEME_LIGHT_COLORBLIND}" checked-on="config.workbench.colorTheme == '${ThemeSettingDefaults.COLOR_THEME_LIGHT_COLORBLIND}'">
+			<img width="200" src="./light-colorblind.png"/>
+			${escape(localize('light-colorblind', "Light Colorblind"))}
+		</checkbox>
+	</div>
+	<div class="theme-picker-row">
 		<checkbox when-checked="setTheme:${ThemeSettingDefaults.COLOR_THEME_DARK}" checked-on="config.workbench.colorTheme == '${ThemeSettingDefaults.COLOR_THEME_DARK}'">
 			<img width="200" src="./dark.png"/>
 			${escape(localize('dark', "Dark Modern"))}
